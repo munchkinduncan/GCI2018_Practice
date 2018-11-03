6af6fdf4a4888e561b191219ed95685de5ddd3bf
unknown <munchkinmainy@gmail.com>
2018-11-03 08:07:36 +0000

    First version of GCI code

---
+++ NameRev.py
@@ -0,0 +1,10 @@
+
+for x in range(0, 10):
+    print ("GCI is Great") 
+    x += 1
+
+print ("What is your name")
+
+newName = input()
+revName = newName [::-1]
+print("Hello",newName,"pleased to meet you, did you know that your name backwards is",revNa
